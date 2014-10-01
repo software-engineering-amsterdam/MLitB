7b48ce256b739c1699064f5312eb46f565a08765
Remco Hendriks <r3mcohendriks@gmail.com>
2014-10-01 09:22:50 +0200

    sort fix

--- app/static/client.js
+++ app/static/client.js
@@ -284,7 +284,7 @@ Client.prototype = {
         }
 
         labeledResults = labeledResults.sort(function(a,b) {
-            return a[1] - b[1];
+            return b[1] - a[1];
         });
 
         console.log(labeledResults);
