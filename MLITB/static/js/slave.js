01a72c670e8ab8f0496ada8b23ebb8775c00f463
Remco Hendriks <r3mcohendriks@gmail.com>
2014-11-26 23:10:51 +0100

    camera function works now

--- MLITB/static/js/slave.js
+++ MLITB/static/js/slave.js
@@ -96,7 +96,7 @@ Slave.prototype = {
         console.log(this.Net);
 
         var Input = new mlitb.Vol(vol_input.sx, vol_input.sy, vol_input.depth, 0.0);
-        Input.data = data;
+        Input.data = data.data;
         this.Net.forward(Input);
         var results = this.Net.getPrediction().data;
 
